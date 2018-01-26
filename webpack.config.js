88da2e924e83d14cf047e0b413641c8773c187bd
Buddy Toups <mydesignbuddy@gmail.com>
2018-01-26 10:38:02 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+  entry: {
+    main: ["./src/index.js"],
+    metro: ["./src/scss/themes/metro/main.scss"],
+    aero: ["./src/scss/themes/aero/main.scss"],
+    cocoa: ["./src/scss/themes/cocoa/main.scss"]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "source-map", // any "source-map"-like devtool is possible
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: '[name].css',
+      allChunks: true,
+    }),
+  ],
+};
